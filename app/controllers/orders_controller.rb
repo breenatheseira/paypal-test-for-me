dfbcc587d9793814090122b3f2e762edbe82f7e3
Breena Theseira <breenatheseira@yahoo.com>
2015-01-12 05:48:34 +0000

    initial commit, active merchant setup and created Order controller

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,13 @@
+class OrdersController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def express_checkout
+  end
+  
+  def make_orders
+  end
+end
---
+++ db/migrate/20150112050823_create_orders.rb
@@ -0,0 +1,11 @@
+class CreateOrders < ActiveRecord::Migration
+  def change
+    create_table :orders do |t|
+      t.string :ip
+      t.string :express_token
+      t.string :express_payer_id
+
+      t.timestamps
+    end
+  end
+end
