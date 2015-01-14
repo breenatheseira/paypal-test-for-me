998217cd80d2018f85fbce28917b9b14fe2a9bfe
Breena Theseira <breenatheseira@yahoo.com>
2015-01-14 10:11:46 +0000

    Added: code for create method.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,10 +4,22 @@ class OrdersController < ApplicationController
   end
 
   def create
+    @order = Order.new(order_params)
+    @order.ip = request.remote_ip
+  
+    if @order.save
+      if @order.purchase # this is where we purchase the order. refer to the model method below
+        redirect_to successful_orders_url(@order)
+      else
+        render :action => "failure"
+      end
+    else
+      render :action => 'new'
+    end    
   end
 
   def express_checkout
-     response = EXPRESS_GATEWAY.setup_purchase(150,
+     response = ::EXPRESS_GATEWAY.setup_purchase(150,
       ip: request.remote_ip,
       return_url: new_order_url,
       cancel_return_url: cancel_orders_url,
@@ -15,7 +27,10 @@ class OrdersController < ApplicationController
       allow_guest_checkout: true,
       items: [{name: "Order", description: "Order description", quantity: "1", amount: 150}]
     )
-    redirect_to EXPRESS_GATEWAY.redirect_url_for(response.token)
+    redirect_to ::EXPRESS_GATEWAY.redirect_url_for(response.token)
+  end
+  
+  def failure
   end
   
   def make_orders
@@ -24,6 +39,11 @@ class OrdersController < ApplicationController
   def cancel
   end
   
-  def success
+  def successful_orders
   end
+  
+  def order_params
+    params.require(:order).permit(:ip, :express_token, :exxpress_payer_id)
+  end
+  
 end
