161e3bb634b6229da54951ae007756de224d90c7
Breena Theseira <breenatheseira@yahoo.com>
2015-01-14 10:10:06 +0000

    Added: Order model methods.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,26 @@
 class Order < ActiveRecord::Base
+  def purchase
+    response = EXPRESS_GATEWAY.purchase(150, express_purchase_options)
+    self.update_attribute(:purchased_at, Time.now) if response.success?
+    response.success?
+  end
+
+  def express_token=(token)
+    self[:express_token] = token
+    if new_record? && !token.blank?
+      # you can dump details var if you need more info from buyer
+      details = EXPRESS_GATEWAY.details_for(token)
+      self.express_payer_id = details.payer_id
+    end
+  end
+
+  private
+
+  def express_purchase_options
+    {
+      :ip => ip,
+      :token => express_token,
+      :payer_id => express_payer_id
+    }
+  end
 end
